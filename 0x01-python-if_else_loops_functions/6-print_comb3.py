9415a2a30b9ef590395715185f3a328feedad353
productivesa <salahzeinu@gmail.com>
2022-09-21 11:13:06 -0700

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-for x in range(0, 10):
-    for j in range(1, 10):
-        if x != j and j > x:
-            if x == 8 and x == 9:
-                print("{0}{1}".format(x, j))
-            else:
-                print("{0}{1}".format(x, j), end=',')
+for x in range(0, 90):
+    if x % 10 > x / 10:
+        if x != 89:
+            print("{:02d}, ".format(x), end='')
+        else:
+            print("{:02d}".format(x))
