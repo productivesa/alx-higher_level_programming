ea33f58c233e39584715847bb530ed49594e7116
productivesa <salahzeinu@gmail.com>
2022-09-21 10:50:43 -0700

    6-print_comb3.py

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for x in range(0, 10):
+    for j in range(1, 10):
+        if x != j and j > x:
+            if x == 8 and x == 9:
+                print("{0}{1}".format(x, j))
+            else:
+                print("{0}{1}".format(x, j), end=', ')
