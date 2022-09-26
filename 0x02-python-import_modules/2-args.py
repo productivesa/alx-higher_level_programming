154b0fba3461c5e114f9ecced1be1b7bc3ccb8ab
productivesa <salahzeinu@gmail.com>
2022-09-26 10:21:21 -0700

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv
+    index = 1
+    counter = len(argv)-1
+    if counter == 1:
+        print("{} argument:".format(counter))
+        print("{}: {}".format(index, argv[counter]))
+    elif counter == 0:
+        print("{} arguments.".format(counter))
+    else:
+        print("{} arguments:".format(counter))
+        while index <= counter:
+            print("{}: {}".format(index, argv[index]))
+            index += 1
