995bcc1cbcbde853eb188c38ddf1e6e13070821c
productivesa <salahzeinu@gmail.com>
2022-10-01 08:09:31 -0700

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if (not isinstance(roman_string, str) or
-            roman_string is None):
-        return (0)
     roman_num = {
             "I": 1,
             "V": 5,
