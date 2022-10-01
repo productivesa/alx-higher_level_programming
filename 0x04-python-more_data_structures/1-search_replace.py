a2cd493166ff593c0a05ddc775e91aa1defc99e4
productivesa <salahzeinu@gmail.com>
2022-10-01 06:45:57 -0700

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-search_replace = __import__('1-search_replace').search_replace
-
-my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
-new_list = search_replace(my_list, 2, 89)
-
-print(new_list)
-print(my_list)
+def search_replace(my_list, search, replace):
+    new_list = my_list[:]
+    for index in range(len(new_list)):
+        if new_list[index] == search:
+            new_list[index] = replace
+    return new_list
