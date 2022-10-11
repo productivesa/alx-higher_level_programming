41c514ddc5bd061af384aa8878ff23ccc1cd8b31
productivesa <salahzeinu@gmail.com>
2022-10-11 02:01:57 -0700

    1-safe_print_integer.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,8 +4,8 @@ def safe_print_list(my_list=[], y=0):
     for x in range(y):
         try:
             print("{}".format(my_list[x]), end='')
-        except:
-            break 
+        except Exception:
+            break
         else:
             lister = lister + 1
     print()
