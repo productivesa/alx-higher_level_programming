fbbb7ff88cc108eec15cd6e48699f62071cd1c28
productivesa <salahzeinu@gmail.com>
2022-10-11 20:40:24 -0700

    4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+
+"""a class Square that defines a
+square by: (based on 3-square.py)
+Instantiation with optional parameter
+size: def __init__(self, size=0):
+size must be an integer, otherwise raise a
+TypeError exception with the message size must be an integer
+if size is less than 0, raise a ValueError exception
+with the message size must be >= 0"""
+
+
 class Square:
     """Includes a Public instance method:
 def area(self): that returns the current square area"""
