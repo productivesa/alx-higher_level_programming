380e7d9bddcbae9a33b30141ed31668fd0a417a5
productivesa <salahzeinu@gmail.com>
2022-10-11 20:38:34 -0700

    3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -5,6 +5,15 @@ square by: (based on 2-square.py)"""
 
 
 class Square:
+    """Instantiation with optional parameter
+    size: def __init__(self, size=0):
+size must be an integer, otherwise raise a TypeError
+exception with the message size must be an integer
+if size is less than 0, raise a ValueError exception
+with the message size must be >= 0
+Includes a Public instance method:
+def area(self): that returns the current square area"""
+
     def __init__(self, size=0):
         if type(size) is int:
             if size < 0:
