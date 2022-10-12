7431bda1330251766a6ffc97efb9b229f713858e
productivesa <salahzeinu@gmail.com>
2022-10-11 19:57:36 -0700

    4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+class Square:
+    """Includes a Public instance method:
+def area(self): that returns the current square area"""
+
+    def __init__(self, size=0):
+        self.size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is int:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = value
+        else:
+            raise TypeError("size must be an integer")
