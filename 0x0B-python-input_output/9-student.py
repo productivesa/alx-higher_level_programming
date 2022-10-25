f2552218aa4ce05b58d5128682efcf9e61ae8456
productivesa <salahzeinu@gmail.com>
2022-10-25 04:54:55 -0700

    9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python3
-""" Module defines class student """
+"""method for student creation"""
 
 
 class Student:
-    """ creating student classs """
+    """Student obj, interesting how you don't have to directly
+    test for strings in a loop, python is weird"""
 
     def __init__(self, first_name, last_name, age):
-        self.firstname = first_name
+        self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        return self.__dict__.copy()
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        dict = {}
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                dict[key] = value
+        return dict
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
