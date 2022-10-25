c565af3eff85e2c7954b6ee00bad8f2536a54082
productivesa <salahzeinu@gmail.com>
2022-10-25 04:43:31 -0700

    8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
-""" Module that returns the dictionary description with a simple
-data structure for a JSON serialization of an object
-"""
 
 
 def class_to_json(obj):
-    """ Function that retuns the dictionary description of an obj """
-
-    di = {}
-    if att(obj, "__dict__"):
-        di = obj.__dict__.copy()
-    return dj
+    return vars(obj)
