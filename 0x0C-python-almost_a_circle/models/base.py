221e85b15cb114d9dc40c9bf147ebde0ac4b3aff
productivesa <salahzeinu@gmail.com>
2022-10-31 10:53:19 -0700

    bas

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-
+"""This is the Base module.
+Contains the Base class which will be the
+“base” of all other classes in this project.
+"""
 import json
 import csv
 import turtle
