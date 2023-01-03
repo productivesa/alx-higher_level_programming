1edef660f3eb3ed4312f5ed4582385ca53a31c3c
productivesa <salahzeinu@gmail.com>
2023-01-03 04:23:25 -0800

    4-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let x = 0; x < this.height; x++) {
+      let s = '';
+      for (let y = 0; y < this.width; y++) {
+        s = s + 'X';
+      }
+      console.log(s);
+    }
+  }
+
+  rotate () {
+    const aux = this.width;
+    this.width = this.height;
+    this.height = aux;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
