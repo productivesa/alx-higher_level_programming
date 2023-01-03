7db2700db1ab518fe2d64f1c6c39efc6867d555d
productivesa <salahzeinu@gmail.com>
2023-01-03 04:32:07 -0800

    7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,9 +2,9 @@
 exports.nbOccurences = function (list, searchElement) {
   let nOccurrences = 0;
   for (let x = 0; x < list.length; x++) {
-    if (searchElement === list[i]) {
+    if (searchElement === list[x]) {
       nOccurrences++;
     }
   }
   return nOccurrences;
-};
+}
