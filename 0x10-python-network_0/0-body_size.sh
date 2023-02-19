4cdad9acba12b6ce077a0b8671f18d0e99453589
productivesa <salahzeinu@gmail.com>
2023-02-19 10:24:34 -0800

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Script that shows the Content-Length from a HTTP request
-curl -sI "$1" | grep "Content-Length:" | cut -d " " -f 2#!/bin/bash
-
+# bash script that takes url,sends a request
+# to url and display size od body response
+curl - sI "$1" | grep "Content-Length:" | cut - d " " - f 2
